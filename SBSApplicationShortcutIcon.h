4623e95312edecdcea484c1223cc66e148f84b24
0xilis <109871561+0xilis@users.noreply.github.com>
2023-01-23 09:47:26 -0500

    SBSApplicationShortcutIcon.h

---
+++ SBSApplicationShortcutIcon.h
@@ -0,0 +1,7 @@
+#import <SpringBoardServices/SBSApplicationShortcutIcon.h>
+
+@interface SBSApplicationShortcutIcon (ShortcutUIKit)
++(id)_scui_defaultImage;
+-(id)_scui_iconImageWithAssetProvider:(id)arg0;
+-(id)scui_iconImageWithAssetProvider:(id)arg0;
+@end
